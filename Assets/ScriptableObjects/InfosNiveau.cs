61fa5adefda562007cc98e54d311c57460acef79
eguerette <emilycloutierg@gmail.com>
2024-12-04 10:53:15 -0500

    Added different prefabs for ennemies

---
+++ Assets/ScriptableObjects/InfosNiveau.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Niveau", menuName = "ScriptableObjects/NiveauInfo")]
+public class InfosNiveau : ScriptableObject
+{
+    public float temps;
+    public float tempsInitial;
+    public float Coins;
+}
--- Assets/Scripts/Enemy/EnemySpawn.cs
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
@@ -8,9 +10,14 @@ public class EnemySpawn : MonoBehaviour
     public GameObject enemy;
     public GameObject enemySpawnPoint;
     public Transform Goal;
+    public InfosNiveau _infoNiveau;
+    private Enemy _enemy;
     private bool spawned = false;
     private float spawnCooldown = 10f;
     private float spawnCooldownReset = 10f;
+    string[] _enemies1 = {"Enemy", "Enemy1"};
+    string[] _enemies2 = {"Enemy", "Enemy1", "Enemy2"};
+    
 
     void Start() {
 
@@ -18,7 +25,34 @@ public class EnemySpawn : MonoBehaviour
     }
 
     void Update()
-    {
+    {   
+
+        if(_infoNiveau.temps <= 5) {
+
+            _enemy._tag = "Enemy";
+        }
+        else if (_infoNiveau.temps > 5 && _infoNiveau.temps <= 10) {
+
+            _enemy._tag = _enemies1[UnityEngine.Random.Range(0, _enemies1.Length)];
+        }
+        else if (_infoNiveau.temps > 10 && _infoNiveau.temps < 15) {
+
+            _enemy._tag = _enemies2[UnityEngine.Random.Range(0, _enemies1.Length)];
+        }
+
+        // if(_infoNiveau.temps <= 100) {
+
+        //     _enemy._tag = "Enemy";
+        // }
+        // else if (_infoNiveau.temps > 100 && _infoNiveau.temps <= 200) {
+
+        //     _enemy._tag = _enemies1[UnityEngine.Random.Range(0, _enemies1.Length)];
+        // }
+        // else if (_infoNiveau.temps > 200 && _infoNiveau.temps < 300) {
+
+        //     _enemy._tag = _enemies2[UnityEngine.Random.Range(0, _enemies1.Length)];
+        // }
+
         if(spawned == false) {
 
             Spawn();
@@ -42,4 +76,5 @@ public class EnemySpawn : MonoBehaviour
         spawned = false;
 
     }
+
 }
