4a31dacad77928d7a3bcbcee362e5b7a529ca5d0
Emile Guerette <emilycloutierg@gmail.com>
2024-12-11 06:17:26 -0500

    goal scripts + sceneswitch

--- Assets/Scripts/Goal/Goal2.cs
+++ Assets/Scripts/Goal/Goal2.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Goal : MonoBehaviour
-{   
+public class Goal2 : MonoBehaviour
+{
     private InfosNiveau _info;
     private void OnTriggerEnter(Collider other) {
 
         if (other.tag == "Enemy" || other.tag == "Enemy2" || other.tag == "Enemy3") {
 
-            _info.Goal1Destroyed = true;
+            _info.Goal2Destroyed = true;
             Destroy(gameObject);
         }
     }
