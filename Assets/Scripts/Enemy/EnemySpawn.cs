a01cb535c41718f7c549982653d4680b0e98bf22
Emile Guerette <emilycloutierg@gmail.com>
2024-12-11 03:21:00 -0500

    Revert "enemy done + spawn lvl 2"

--- Assets/Scripts/Enemy/EnemySpawn.cs
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -1,3 +1,55 @@
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using Unity.Mathematics;
+// using Unity.VisualScripting;
+// using UnityEngine;
+
+// public class EnemySpawn : MonoBehaviour
+// {   
+//     public GameObject enemy;
+//     public GameObject enemy1;
+//     public GameObject enemy2;
+//     public GameObject enemySpawnPoint;
+//     public Transform Goal;
+//     public InfosNiveau _infoNiveau;
+//     private bool spawned = false;
+//     private float spawnCooldown = 10f;
+//     private float spawnCooldownReset = 10f;
+    
+
+//     void Start() {
+
+//         gameObject.transform.LookAt(Goal.position);
+//     }
+
+//     void Update()
+//     {   
+//         if(spawned == false) {
+
+//             SpawnLvl1();
+//         }
+//     }
+
+//     private void SpawnLvl1() {
+
+//         Transform _enemy = Instantiate(enemy.transform, enemySpawnPoint.transform.position, quaternion.identity);
+//         _enemy.transform.rotation = enemySpawnPoint.transform.rotation;
+        
+//         spawnCooldown = spawnCooldownReset;
+//         spawned = true;
+
+//         StartCoroutine(SpawnRate());
+//     }
+
+//     private IEnumerator SpawnRate() {
+
+//         yield return new WaitForSeconds(spawnCooldown);
+//         spawned = false;
+
+//     }
+
+// }
 using System.Collections;
 using UnityEngine;
 
