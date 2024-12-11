2364792ed8238e514685bac00feee72d576adb3e
Emile Guerette <emilycloutierg@gmail.com>
2024-12-11 05:47:36 -0500

    done

---
+++ Assets/Scripts/PointAffichageFin.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PointAffichageFin : MonoBehaviour
+{
+    [SerializeField] private InfosNiveau _info;
+    [SerializeField] private TMP_Text points;
+
+    void Start() {
+
+        points.text = "Score: " + _info.Coins;
+    }
+}
